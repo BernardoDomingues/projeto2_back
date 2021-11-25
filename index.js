454c42e489bdfe423f57ba9e87c18a16ff6e8ef1
Bernardo Domingues <bernardoopd@hotmail.com>
2021-11-25 11:16:20 -0300

    retira referencia de cors

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const express = require("express");
 // Importa Controllers
 const { version } = require("./Controller/version");
 const { register } = require("./Controller/register");
-const cors = require("./config/cors");
 const PORT = 5000; // Porta
 
 // Configurações do app
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
 
 // Rotas
 app.get("/version", version);
