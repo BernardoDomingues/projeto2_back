35b2a89963918eac30e25d557bbe1b3966e387e6
Bernardo Domingues <bernardoopd@hotmail.com>
2021-11-25 11:19:47 -0300

    adiciona rota de conferencia

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
-app.get("/version", version);
+app.get("/", version);
 app.post("/register", register);
 
 // Declaração do App
