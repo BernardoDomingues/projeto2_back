43006b891c33ded665a67b6012685e0fd3d1f7c5
Bernardo Domingues <bernardoopd@hotmail.com>
2021-11-29 15:30:40 -0300

    adiciona dados de usuário ao fazer o login

--- services/login.js
+++ services/login.js
@@ -2,13 +2,16 @@ const { users } = require("../users");
 
 module.exports.loginService = (body) => {
   let loginStatus = false;
+  let userData = {}
   for (const user of users) {
     if (user.email === body.email && user.password === body.password) {
       loginStatus = true;
+      userData = user;
     }
   }
   return {
     status: loginStatus,
+    userData: userData,
     description: "Os dados foram recebidos com sucesso.",
   };
 };
