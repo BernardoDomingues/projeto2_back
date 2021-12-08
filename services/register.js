16191d045ceb4fae2aaa7ba56a6b39eeb3b59e3e
Vinicius Santos <viniciusmain1@gmail.com>
2021-12-08 20:57:08 -0300

    criptografia pronta

--- services/register.js
+++ services/register.js
@@ -2,8 +2,13 @@ const firebase = require("firebase");
 const config = require("../config/database");
 const app = firebase.initializeApp(config);
 const db = firebase.firestore(app);
+const bcrypt = require("bcryptjs"); //Criptografia da senha
 
 module.exports.registerService = async (body) => {
+const saltRounds = 10;
+const salt = await bcrypt.genSaltSync(saltRounds);
+body.password = bcrypt.hashSync(body.password,salt);
+body.passwordConfirmation = bcrypt.hashSync(body.passwordConfirmation,salt);
   const saveDatabase = await db.collection("users").doc().set(body);
   return {
     status: true,
