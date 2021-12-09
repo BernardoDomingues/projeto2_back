34566fcb2d4ce6349f936a40c8bd616939083b43
Vinicius Santos <viniciusmain1@gmail.com>
2021-12-08 21:40:12 -0300

    retira console

--- Controller/moviesData.js
+++ Controller/moviesData.js
@@ -4,7 +4,6 @@ const moviesData = async (req, res) => {
     const { body } = req
     try {
         const response = await getMoviesData(body.reqType, body.options);
-        console.log(response);
         return res.status(200).json(response);
       } catch (error) {
         return res.status(error.status || 500).json({
